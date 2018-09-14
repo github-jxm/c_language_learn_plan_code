cdcb5f44e0b62d546a3cb536260af21566cdf0b5
jxm <cjiangxumin@gmail.com>
2018-09-14 19:52:34 +0800

    update ...

--- 4/2/locker/dlist.h
+++ 4/2/locker/dlist.h
@@ -1,33 +1,5 @@
 /*
  * File:    dlist.h
- * Author:  Li XianJing <xianjimli@hotmail.com>
- * Brief:   double list header file.
- *
- * Copyright (c) Li XianJing
- *
- * Licensed under the Academic Free License version 2.1
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- */
-
-/*
- * History:
- * ================================================================
- * 2008-11-24 Li XianJing <xianjimli@hotmail.com> created
- * 2008-12-08 Li XianJing <xianjimli@hotmail.com> add autotest.
- * 2008-12-10 Li XianJing <xianjimli@hotmail.com> add lock support.
  */
 
 #include <stdio.h>
