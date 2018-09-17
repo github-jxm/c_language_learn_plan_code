f1f29eb78fb2137cd85221d146ccb6dcb7577dbf
jiangxumin <cjiangxumin@gmail.com>
2018-09-17 08:54:55 +0000

    update ...

--- 5/2/stack/typedef.h
+++ 5/2/stack/typedef.h
@@ -1,32 +1,5 @@
 /*
  * File:    typedef.h
- * Author:  Li XianJing <xianjimli@hotmail.com>
- * Brief:   common types definition.
- *
- * Copyright (c) Li XianJing
- *
- * Licensed under the Academic Free License version 2.1
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- */
-
-/*
- * History:
- * ================================================================
- * 2008-12-10 Li XianJing <xianjimli@hotmail.com> created.
- *
  */
 
 #include <stdio.h>
@@ -70,4 +43,3 @@ typedef int       (*DataHashFunc)(void* data);
 typedef Ret (*SortFunc)(void** array, size_t nr, DataCompareFunc cmp);
 
 #endif/*TYPEDEF_H*/
-
