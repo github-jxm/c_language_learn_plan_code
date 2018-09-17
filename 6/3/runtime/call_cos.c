e81d097cc5c861106f67c3c65ad118fe445cce76
jiangxumin <cjiangxumin@gmail.com>
2018-09-17 16:45:02 +0000

    update ....

--- 6/3/runtime/call_cos.c
+++ 6/3/runtime/call_cos.c
@@ -1,3 +1,7 @@
+/*
+ * File : call_cos.c
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <dlfcn.h>
--- 6/3/runtime/module_test.c
+++ 6/3/runtime/module_test.c
@@ -1,24 +1,24 @@
+/*
+ * File : module_test.c
+ */
+
 #include <stdio.h>
 #include "module.h"
 
 int main(int argc, char* argv[])
 {
-	if(argc != 3)
-	{
+	if(argc != 3) {
 		printf("%s module function\n", argv[0]);
 
 		return 0;
 	}
 
 	Module* module = module_create(argv[1], 0);
-	if(module != NULL)
-	{
+	if(module != NULL) {
 		void* func = module_sym(module, argv[2]);
 		printf("func=%p\n", func);
 		module_destroy(module);
-	}
-	else
-	{
+	} else {
 		printf("load %s failed\n", argv[1]);
 	}
 
