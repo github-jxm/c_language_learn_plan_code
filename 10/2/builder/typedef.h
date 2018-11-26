c6a22d5d5e17a3d1af6af2056dad79be9102dc3b
jiangxumin <cjiangxumin@gmail.com>
2018-11-26 13:06:08 +0000

    update ...

--- 10/2/builder/typedef.h
+++ 10/2/builder/typedef.h
@@ -1,32 +1,5 @@
 /*
  * File:    typedef.h
- * Author:  Li XianJing <xianjimli@hotmail.com>
- * Brief:   common types definition.
- *
- * Copyright (c) Li XianJing
- *
- * Licensed under the Academic Free License version 2.1
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- */
-
-/*
- * History:
- * ================================================================
- * 2008-12-10 Li XianJing <xianjimli@hotmail.com> created.
- *
  */
 
 #include <stdio.h>
@@ -45,10 +18,10 @@ typedef enum _Ret
 	RET_FAIL
 }Ret;
 
-typedef void     (*DataDestroyFunc)(void* ctx, void* data);
-typedef int      (*DataCompareFunc)(void* ctx, void* data);
-typedef Ret      (*DataVisitFunc)(void* ctx, void* data);
-typedef int       (*DataHashFunc)(void* data);
+typedef void  (*DataDestroyFunc)(void* ctx, void* data);
+typedef int   (*DataCompareFunc)(void* ctx, void* data);
+typedef Ret   (*DataVisitFunc)(void* ctx, void* data);
+typedef int   (*DataHashFunc)(void* data);
 
 #ifdef __cplusplus
 #define DECLS_BEGIN extern "C" {
@@ -71,6 +44,4 @@ typedef Ret (*SortFunc)(void** array, size_t nr, DataCompareFunc cmp);
 
 #define MAX_ATTR_NR 64
 
-
 #endif/*TYPEDEF_H*/
-
